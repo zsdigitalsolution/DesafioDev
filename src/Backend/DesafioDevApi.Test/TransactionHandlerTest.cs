1e654709af9dd3282e6f2145b65d9793342d896c
Felipe Serra <contatozsdigital@gmail.com>
2023-07-20 22:20:31 -0300

    Adicionando o teste unitário do handler get, getall e sendfile

--- src/Backend/DesafioDevApi.Test/TransactionHandlerTest.cs
+++ src/Backend/DesafioDevApi.Test/TransactionHandlerTest.cs
@@ -3,7 +3,10 @@ using DesafioDevApi.Domain.Commands.Outputs;
 using DesafioDevApi.Domain.Contract;
 using DesafioDevApi.Domain.Entities;
 using DesafioDevApi.Domain.Handlers;
+using DesafioDevApi.Infrastructure.Data.Common;
+using DesafioDevApi.Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace DesafioDevApi.Test
@@ -12,7 +15,8 @@ namespace DesafioDevApi.Test
     public class TransactionHandlerTests
     {
         private Mock<ITransactionService> _serviceMock;
-        private Mock<ITransactionRepository> _repositoryMock;       
+        private ApiDBContext _dbContext;
+        private TransactionRepository _repository;
         private Mock<IUnitOfWork> _unitOfWorkMock;
         private TransactionFileHandler _fileHandler;
         private TransactionGetAllHandler _getAllHandler;
@@ -20,13 +24,20 @@ namespace DesafioDevApi.Test
 
         [TestInitialize]
         public void TestInitialize()
-        {    _serviceMock = new Mock<ITransactionService>();
-            _repositoryMock = new Mock<ITransactionRepository>();
-            
+        {
+            _serviceMock = new Mock<ITransactionService>();
+            // Create in-memory database
+            var options = new DbContextOptionsBuilder<ApiDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _dbContext = new ApiDBContext(options);
+            _repository = new TransactionRepository(_dbContext);
+
             _unitOfWorkMock = new Mock<IUnitOfWork>();
-            _fileHandler = new TransactionFileHandler(_repositoryMock.Object, _serviceMock.Object, _unitOfWorkMock.Object);
-            _getAllHandler = new TransactionGetAllHandler(_repositoryMock.Object);
-            _getHandler = new TransactionGetHandler(_repositoryMock.Object);
+            _fileHandler = new TransactionFileHandler(_repository, _serviceMock.Object, _unitOfWorkMock.Object);
+            _getAllHandler = new TransactionGetAllHandler(_repository);
+            _getHandler = new TransactionGetHandler(_repository);
         }
         private Transaction TransactionFake()
         {
@@ -47,7 +58,7 @@ namespace DesafioDevApi.Test
         {
             // Arrange
             var fileMock = new Mock<IFormFile>();
-            var content = "1201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       ";
+            var content = "1201903010000014200096206760174753***3153153453JOÃO MACEDO   BAR DO JOÃO       ";
             var fileName = "test.txt";
             var ms = new MemoryStream();
             var writer = new StreamWriter(ms);
@@ -62,8 +73,8 @@ namespace DesafioDevApi.Test
             var request = new TransactionFileRequestCommand(file: file);
             var transactions = new List<Transaction> { TransactionFake(), TransactionFake() };
             _serviceMock.Setup(s => s.ParseCNABFileAsync(request.File)).ReturnsAsync(transactions);
-            _repositoryMock.Setup(r => r.AddAsync(transactions)).Returns(Task.CompletedTask);
-            _unitOfWorkMock.Setup(u => u.Commit()).Returns(true);
+            await _repository.AddAsync(transactions);
+            await _dbContext.SaveChangesAsync();
 
             // Act
             var response = await _fileHandler.Handle(request, CancellationToken.None);
@@ -75,29 +86,31 @@ namespace DesafioDevApi.Test
         [TestMethod]
         public async Task TestTransactionGetAllHandler()
         {
-            // Arrange
+            // Preparação
             var request = new TransactionGetAllRequestCommand();
-            var transactions = new List<Transaction> { TransactionFake(), TransactionFake() };
-            _repositoryMock.Setup(r => r.AddAsync(transactions)).Returns(Task.CompletedTask);
-            _unitOfWorkMock.Setup(u => u.Commit()).Returns(true);
-            
-            _repositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(transactions);
+            var transactions = new List<Transaction> {  TransactionFake(), TransactionFake() };
 
-            // Act
+            await _repository.AddAsync(transactions);
+            await _dbContext.SaveChangesAsync();
+
+            // Ação
             var response = await _getAllHandler.Handle(request, CancellationToken.None);
 
-            // Assert
+            // Verificação
             Assert.IsTrue(!response.HasMessages);
             Assert.AreEqual(transactions.Count, ((IEnumerable<TransactionResponseCommand>)response.Value).Count());
         }
 
+
+
         [TestMethod]
         public async Task TestTransactionGetHandler()
         {
-            // Arrange
-            var request = new TransactionGetRequestCommand(id: 1);
+            // Arrange            
             var transaction = TransactionFake();
-            _repositoryMock.Setup(r => r.GetByIdAsync<Transaction>(x => x.Id == request.Id, null, false)).ReturnsAsync(transaction);
+            var request = new TransactionGetRequestCommand(id: transaction.Id);
+            await _repository.AddAsync(transaction);
+            await _dbContext.SaveChangesAsync();
 
             // Act
             var response = await _getHandler.Handle(request, CancellationToken.None);
@@ -106,7 +119,5 @@ namespace DesafioDevApi.Test
             Assert.IsTrue(!response.HasMessages);
             Assert.AreEqual(transaction.Id, ((TransactionResponseCommand)response.Value).Id);
         }
-
-
     }
 }
